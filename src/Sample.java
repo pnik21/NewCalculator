a63a3eff20e0488779c547d0be47d1bedbd2575e
sir <sir@DESKTOP-KSQ6RIJ>
2022-05-27 18:23:56 +0530

    This is new Project

---
+++ src/Sample.java
@@ -0,0 +1,41 @@
+
+public class Sample 
+{
+
+	
+	int x=10;
+	int y=20;
+
+
+	public void add()
+	{
+		
+		
+		
+		
+		
+	}
+
+	public void sub()
+	{
+		
+		
+		
+		
+		
+	}
+		
+		
+		public static void main(String[] args)
+		{
+		
+			
+			
+			
+			
+		}
+	
+	
+	
+	
+}
